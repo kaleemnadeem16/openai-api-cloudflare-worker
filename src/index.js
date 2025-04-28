79bbd57e2d5dd500fed7bd5c1a8cb2e85d7d6f2f
Your Name <you@example.com>
2025-04-28 18:20:45 +0500

    added localhost capability

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import OpenAI from 'openai';
-
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type',
+}
 export default {
 	async fetch(request, env, ctx) {
+		if (request.method === 'OPTIONS') {
+			return new Response(null, { headers: corsHeaders });
+		}
 		const openai = new OpenAI({
 			apiKey: env.OPENAI_API_KEY,
+			baseURL: 'https://gateway.ai.cloudflare.com/v1/8c232f08ab338de346cf66f7809ce517/openiai-api-cloudflare-worker/openai'
 		})
 		try{
-			const response = await openai.chat.completions.create({
+			const messages = await request.json()
+			const chatCompletion = await openai.chat.completions.create({
 				model: 'gpt-4.1-nano',
-				messages: [
-					{ role: 'user', content: 'What is the most nearest holiday in Pakistan?' },
-				],
+				messages,
 			});
-			return new Response(JSON.stringify(response));
+			const response =  chatCompletion.choices[0].message;
+			return new Response(JSON.stringify(response), {headers: corsHeaders});
 		}
-		catch (error) {	
-			console.error('Error:', error);
-			return new Response('Error: ' + error.message, { status: 500 });
+		catch (e) {	
+			return new Response(JSON.stringify({ error: e.message }), { status: 500, headers: corsHeaders });
 		}
 		
 		
