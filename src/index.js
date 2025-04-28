8f6e427c82b20c66013ea8e857c748ea0dcc3db3
Your Name <you@example.com>
2025-04-28 13:58:21 +0500

    3rd commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export default {
 			const response = await openai.chat.completions.create({
 				model: 'gpt-4.1-nano',
 				messages: [
-					{ role: 'user', content: 'What is the most nearest holiday!' },
+					{ role: 'user', content: 'What is the most nearest holiday in Pakistan?' },
 				],
 			});
 			return new Response(JSON.stringify(response));
